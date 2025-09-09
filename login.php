0a8c2b49c3a051bf0ea65dd7eb04f77fb25f14c0
unknown <LouisChai8>
2025-09-09 14:28:34 +0700

    feat:implement navbar

---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<header>
+    <img src="gambar/EEA Red.png" alt="logo">
+    <div class="navbar">
+        <div class="link">
+            <a href="home.php"><h4>Home</h4></a>
+            <a href="jadwal.php"><h4>Jadwal Lapangan</h4></a>
+            <div class="garis"></div>
+            <a href="register.php"><h4>Register</h4></a>
+        </div>
+
+        <div class="login"><a href="login.php"><h4>Log in</h4></a></div>  
+    </div>    
+</header>
+
+<body>
+    <div class="kotaklogin">
+        <img src="gambar/gambar login.png" alt="voli">
+        <div class="isikotak">
+            <div class="textbox">
+                <div class="tombollogin">
+                    <div class="textbawah">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
