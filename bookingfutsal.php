42c07387c39a7ac376483aa79d43c3547ae1f281
unknown <LouisChai8>
2025-10-14 16:23:26 +0700

    feat: develop booking page

---
+++ bookingfutsal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="home.css"> 
+</head>
+
+<header>
+    <img src="gambar/EEA Red.png" alt="logo">
+    <div class="navbar">
+        <div class="link">
+            <a href="home.php"><h4>Home</h4></a>
+            <a href="jadwal.php"><h4>Jadwal Lapangan</h4></a>
+            <div class="garis"></div>
+            <a href="register.php"><h4>Register</h4></a>
+        </div>
+
+        <div class="login"><a href="login.php"><h4>Log in</h4></a></div>  
+    </div>    
+</header>
