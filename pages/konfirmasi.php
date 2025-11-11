eac3a82b2d67fa61cd0adeaf4bf2bfe3e2328ae9
Laragoons <elvanfirdellia@gmail.com>
2025-11-11 22:19:52 +0700

    feat: implement admin panel backend

--- pages/konfirmasi.php
+++ pages/konfirmasi.php
@@ -67,22 +67,35 @@ $harga_tampil = "Rp " . number_format($total_harga, 0, ',', '.');
     <div class="navbar">
         <div class="link">
             <a href="home.php"><h4>Home</h4></a>
-            <div class="dropdown">
-                <a href="#" class="dropdown-toggle">
+            
+            <div class="dropdown"> 
+                <a href="#" class="dropdown-toggle"> 
                     <h4>Jadwal Lapangan</h4>
                 </a>
-                <ul class="dropdown-menu">
-                    <li><a href="jadwalfutsal.php">Jadwal Futsal</a></li>
-                    <li><a href="jadwalbasket.php">Jadwal Basket</a></li>
-                    <li><a href="jadwalbadminton.php">Jadwal Badminton</a></li>
-                    <li><a href="jadwalvoli.php">Jadwal Voli</a></li>
+                <ul class="dropdown-menu"> 
+                    <li><a href="jadwal.php?id=1">Jadwal Futsal</a></li>
+                    <li><a href="jadwal.php?id=2">Jadwal Basket</a></li>
+                    <li><a href="jadwal.php?id=3">Jadwal Badminton</a></li>
+                    <li><a href="jadwal.php?id=4">Jadwal Voli</a></li>
                 </ul>
             </div>
-            <div class="garis"></div>
-            <a href="register.php"><h4>Register</h4></a>
+
+            <?php if (!isset($_SESSION['user_id'])): ?>
+                <div class="garis"></div>
+                <a href="register.php"><h4>Register</h4></a>
+            <?php endif; ?>
         </div>
-        <div class="login"><a href="login.php"><h4>Log in</h4></a></div>
-    </div>
+        
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <div class="login">
+                <a href="login.php"><h4>Log in</h4></a>
+            </div>
+        <?php else: ?>
+            <div class="login"> 
+                <a href="../db-pages/logout.php"><h4>Logout</h4></a> 
+            </div>
+        <?php endif; ?>
+    </div>    
 </header>
 
 <body>
